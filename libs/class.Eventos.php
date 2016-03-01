9ea0638f12e3b63b4813f9c2070f4bb7ec877aaf
Carlos García <carlos.agh@gmail.com>
2016-03-01 15:26:33 -0600

    Gafete

--- libs/class.Eventos.php
+++ libs/class.Eventos.php
@@ -1554,7 +1554,7 @@ class Eventos extends Model
 		if (isset($registro->foto_fotografia))
 		{
 			//echo '<img src="data:' . $registro->foto_mime . ';base64,' . $registro->foto_fotografia . '" />';
-			$pdf->Image('@' . base64_decode($registro->foto_fotografia), $x='2', $y='3', 3, 3.2); //  40, 42
+			$pdf->Image('@' . base64_decode($registro->foto_fotografia), $x='3', $y='1', 3, 3.2); //  40, 42
 		}
 
 		// define barcode style
@@ -1576,7 +1576,7 @@ class Eventos extends Model
 
 		// Codigo de barra
 		//$pdf->write1DBarcode($func->nombreImagenBarcode($registro->id_registro), 'C128A', $x='-140', $y='98', '', 16, 0.4, $style, 'N');
-		$pdf->write1DBarcode($func->nombreImagenBarcode($registro->id_registro), 'C128A', $x='3', $y='4.5', '', 2, 0.1, $style, 'N');
+		$pdf->write1DBarcode($func->nombreImagenBarcode($registro->id_registro), 'C128A', $x='2', $y='3.5', '', 1.2, 0.1, $style, 'N');
 
 		// ---------------------------------------------------------
 
