1cd9aabf2dc20381ee699ce7168e5ea99528b9b5
Carlos García <carlos.agh@gmail.com>
2017-08-22 23:15:34 -0500

    gafete
    listo

--- libs/class.Eventos.php
+++ libs/class.Eventos.php
@@ -1538,8 +1538,8 @@ class Eventos extends Model
 		$pdf->writeHTMLCell('90', '', $x='5', $y='60', $nombreApp, $border=0, $ln=0, $fill=0, $reseth=true, $align='C', $autopadding=false);
 		$pdf->writeHTMLCell('90', '', $x='115', $y='60', $nombreApp, $border=0, $ln=0, $fill=0, $reseth=true, $align='C', $autopadding=false);
 		$pdf->SetFont('helvetica', 'B', 18);
-		$pdf->writeHTMLCell('90', '', $x='5', $y='65', $cargo, $border=0, $ln=0, $fill=0, $reseth=true, $align='C', $autopadding=false);
-		$pdf->writeHTMLCell('90', '', $x='115', $y='65', $cargo, $border=0, $ln=0, $fill=0, $reseth=true, $align='C', $autopadding=false);
+		$pdf->writeHTMLCell('90', '', $x='5', $y='70', $cargo, $border=0, $ln=0, $fill=0, $reseth=true, $align='C', $autopadding=false);
+		$pdf->writeHTMLCell('90', '', $x='115', $y='70', $cargo, $border=0, $ln=0, $fill=0, $reseth=true, $align='C', $autopadding=false);
 
 		$pdf->SetFont('helvetica', '', 20);
 		//$pdf->writeHTMLCell('100', '', $x='-15', $y='90', $apellidos, $border=0, $ln=0, $fill=0, $reseth=true, $align='C', $autopadding=false);
