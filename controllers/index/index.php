4be3a65675f14c7c5f2a804fbdaa67abebc30a04
Carlos García <carlos.agh@gmail.com>
2017-10-24 09:48:54 -0500

    app
    listo

--- controllers/index/index.php
+++ controllers/index/index.php
@@ -15,8 +15,7 @@ class Index_Controller extends Controller
 
 	private $seccion = "";
 
-	public function __construct()
-	{
+	public function __construct() {
 		$this->librarys("Sessions", "sess");
 		$this->sess->isSession();
 		$this->model = $this->model("Index");
@@ -29,8 +28,7 @@ class Index_Controller extends Controller
 	/*
 	 * Muestra el formulario de acceso al sistema
 	 */
-	public function inicio()
-	{
+	public function inicio() {
 		// Mostramos la vista
 		$this->display(
 			array(
@@ -42,10 +40,142 @@ class Index_Controller extends Controller
 		);
 	}
 
-	public function rfid($request)
-	{
+	public function rfid($request) {
 		$this->model->prueba($request->query);
 		echo 'Insert!';
+	}
+
+	public function search($request) {
+		header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Credentials: true ");
+		header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+		header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+
+		if (!empty($request->get["paterno"]) && !empty($request->get["nombre"])) {
+			$registros = $this->model->searchAmbos($request->get["paterno"], $request->get["nombre"]);
+		} else if (!empty($request->get["paterno"]) && empty($request->get["nombre"])) {
+			$registros = $this->model->searchPaterno($request->get["paterno"]);
+		} else if (!empty($request->get["nombre"]) && empty($request->get["paterno"])) {
+			$registros = $this->model->searchNombre($request->get["nombre"]);
+		} else if (!empty($request->get["empresa"]) && empty($request->get["paterno"]) && empty($request->get["nombre"])) {
+			$registros = $this->model->searchEmpresa($request->get["empresa"]);
+		}
+
+		echo json_encode($registros);
+	}
+
+	public function get($request) {
+		header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Credentials: true ");
+		header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+		header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+
+		$registro = $this->model->get($request->get["id"]);
+		echo json_encode($registro);
+	}
+
+	public function create($request) {
+		$this->json =  array(
+			"status" => false
+		);
+		header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Credentials: true ");
+		header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+		header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+		$request_body = file_get_contents('php://input');
+		$data = json_decode($request_body);
+
+		if (($id = $this->model->set($data)) !== false) {
+			//$this->base64_to_jpg($data->photo, "./views/images/fotos/foto_" . $id . ".jpg");
+			//$this->base64_to_jpg($data->uid, "./views/images/fotos/id_front_" . $id . ".jpg");
+
+			//$this->model->asistencia($id);
+
+			$this->json["status"] = true;
+			$this->json["id"] = $id;
+			echo json_encode($this->json);
+		}
+	}
+
+	public function update($request) {
+		$this->json =  array(
+			"status" => false
+		);
+		header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Credentials: true ");
+		header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+		header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+		$request_body = file_get_contents('php://input');
+		$data = json_decode($request_body);
+
+		if ($this->model->update($data)) {
+			/*if (!empty($data->photo)) {
+				$this->base64_to_jpg($data->photo, "./views/images/fotos/foto_" . $data->id_registro . ".jpg");
+			}
+			
+			if (!empty($data->uid)) {
+				$this->base64_to_jpg($data->uid, "./views/images/fotos/id_front_" . $data->id_registro . ".jpg");
+			}*/
+
+			$this->json["status"] = true;
+			echo json_encode($this->json);
+		}
+	}
+
+	public function printing($request) {
+		$this->json =  array(
+			"status" => false
+		);
+		header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Credentials: true ");
+		header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+		header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+
+		if ($this->model->asistencia($request->get["id"])) {
+			$this->json["status"] = true;
+			echo json_encode($this->json);
+		}
+	}
+
+	public function validarFolio($request) {
+		header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Credentials: true ");
+		header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+		header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+
+		$this->json = array(
+			"exists" => false
+		);
+
+		if (!empty($request->get["id"])) {
+			if (($registro = $this->model->validarFolioId($request->get["folio"], $request->get["id"])) !== false) {
+				$this->json["exists"] = true;
+			}
+		} else {
+			if (($registro = $this->model->validarFolio($request->get["folio"])) !== false) {
+				$this->json["exists"] = true;
+			}
+		}
+
+		echo json_encode($this->json);
+	}
+
+	private function base64_to_jpg($base64_string, $output_file) {
+		// open the output file for writing
+		$ifp = fopen( $output_file, 'wb' ); 
+
+		// split the string on commas
+		// $data[ 0 ] == "data:image/png;base64"
+		// $data[ 1 ] == <actual base64 string>
+		$data = explode( ',', $base64_string );
+
+		// we could add validation here with ensuring count( $data ) > 1
+		fwrite( $ifp, base64_decode( $data[ 1 ] ) );
+
+		// clean up the file resource
+		fclose( $ifp ); 
+
+		return $output_file;
 	}
 }
 ?>
--- models/index/index.php
+++ models/index/index.php
@@ -12,8 +12,286 @@ use SimpleMVC\Model\Model_Controller as Model;
 
 class Index_Model extends Model
 {
-	public function prueba($post)
-	{
+	public function searchPaterno($name) {
+		$registers = array();
+
+		$qry = "SELECT id_registro, titulo, nombre, app, apm, emp_o_ins, status, impresion_gafete 
+			FROM smc_reg_CIPE14 
+			WHERE app LIKE '%" . $name . "%' 
+			ORDER BY app, apm, nombre ASC
+			LIMIT 50";
+
+		if ($this->db->hQuery($qry)) {
+			if ($this->db->hNumRows() >= 1) {
+				while ($register = $this->db->hFetchObject()) {
+					$registers[] = $register;
+				}
+			}
+		}
+
+		return $registers;
+	}
+
+	public function searchNombre($name) {
+		$registers = array();
+		
+		$qry = "SELECT id_registro, titulo, nombre, app, apm, emp_o_ins, status, impresion_gafete 
+			FROM smc_reg_CIPE14 
+			WHERE nombre LIKE '%" . $name . "%' 
+			ORDER BY app, apm, nombre ASC
+			LIMIT 50";
+
+		if ($this->db->hQuery($qry)) {
+			if ($this->db->hNumRows() >= 1) {
+				while ($register = $this->db->hFetchObject()) {
+					$registers[] = $register;
+				}
+			}
+		}
+
+		return $registers;
+	}
+
+	public function searchEmpresa($name) {
+		$registers = array();
+		
+		$qry = "SELECT id_registro, titulo, nombre, app, apm, emp_o_ins, status, impresion_gafete 
+			FROM smc_reg_CIPE14 
+			WHERE emp_o_ins LIKE '%" . $name . "%' 
+			ORDER BY app, apm, nombre ASC
+			LIMIT 50";
+
+		if ($this->db->hQuery($qry)) {
+			if ($this->db->hNumRows() >= 1) {
+				while ($register = $this->db->hFetchObject()) {
+					$registers[] = $register;
+				}
+			}
+		}
+
+		return $registers;
+	}
+
+	public function searchAmbos($paterno, $nombre) {
+		$registers = array();
+		
+		$qry = "SELECT id_registro, titulo, nombre, app, apm, emp_o_ins, status, impresion_gafete 
+			FROM smc_reg_CIPE14 
+			WHERE app LIKE '%" . $paterno . "%' 
+			AND nombre LIKE '%" . $nombre . "%' 
+			ORDER BY app, apm, nombre ASC
+			LIMIT 50";
+
+		if ($this->db->hQuery($qry)) {
+			if ($this->db->hNumRows() >= 1) {
+				while ($register = $this->db->hFetchObject()) {
+					$registers[] = $register;
+				}
+			}
+		}
+
+		return $registers;
+	}
+
+	public function get($registerId) {
+		$register = array();
+		if (!empty($registerId)) {
+			$qry = "SELECT * 
+				FROM smc_reg_CIPE14 
+				WHERE id_registro = " . $registerId .  "
+				LIMIT 1";
+
+			if ($this->db->hQuery($qry)) {
+				if ($this->db->hNumRows() == 1) {
+					$register = $this->db->hFetchObject();
+				}
+			}
+		}
+		return $register;
+	}
+
+	public function set($reg) {
+		$folio = time();
+		$qry = "INSERT INTO smc_reg_CIPE14 SET 
+			`folio_registro` = %d ,
+			`folio_pago` = '%s' ,
+			`clave_evento` = '%s' ,
+			`clave_registro` = '%s' ,
+			`cat_registro` = '%s' ,
+			`idioma` = '%s' ,
+			`clave_asociada` = '%s' ,
+			`id_costo` = %d ,
+			`costo_registro` = '%s' ,
+			`costo_adicional` = '0.00' ,
+			`costo_total` = '%s' ,
+			`divisa` = '%s' ,
+			`tipo_cambio_divisa` = '%s' ,
+			`impresion_constancia` = %d ,
+
+			`genero` = '%s' ,
+			`titulo` = '%s' ,
+			`nombre` = UPPER('%s') ,
+			`app` = UPPER('%s') ,
+			`apm` = UPPER('%s') ,
+			`telefono_particular` = '%s' ,
+			`rfc` = UPPER('%s') ,
+
+			`emp_o_ins` = UPPER('%s') ,
+			`cargo` = '%s' ,
+
+			`comentarios` = '%s' ,
+			`email` = '%s' ,
+
+			`politicas_terminos_condiciones` = 1 ,
+			`forma_pago` = '%s' ,
+			`fecha_registro` = '%s' ,
+			`status` = '%s' ,
+			`realizado_en` = 'STO'
+		";
+
+		$qry = sprintf($qry,
+			$folio,
+			$folio,
+			"EVEN15",
+			$folio,
+			$reg->categoria,
+			'es',
+			$reg->clave_asociada,
+			5,
+			0,
+			0,
+			"MXN",
+			0,
+			$reg->impresion_constancia,
+
+			"",
+			$reg->titulo,
+			$reg->firstName,
+			$reg->lastName,
+			$reg->secondLastName,
+			$reg->telefono,
+			"",
+			$reg->empresa,
+			"",
+
+			$reg->comentarios,
+			$reg->email,
+
+			"EF",
+			date("Y-m-d H:s:i"),
+			$reg->status
+		);
+
+		if ($this->db->hQuery($qry))
+		{
+			if ($this->db->hAffectedRows() == 1)
+			{
+				return $this->db->hInsertID();
+			}
+		}
+
+		return false;
+	}
+
+	public function update($reg) {
+		$qry = "UPDATE smc_reg_CIPE14 SET 
+			`cat_registro` = '%s' ,
+			`titulo` = '%s' ,
+			`nombre` = UPPER('%s') ,
+			`app` = UPPER('%s') ,
+			`apm` = UPPER('%s') ,
+			`emp_o_ins` = UPPER('%s') ,
+			`telefono_particular` = '%s' ,
+			`comentarios` = '%s' ,
+			`email` = '%s' ,
+			`clave_asociada` = '%s' ,
+			`fecha_modificacion` = '%s' ,
+			`status` = '%s'
+			WHERE id_registro = %d 
+			LIMIT 1
+		";
+
+		$qry = sprintf($qry,
+			$reg->cat_registro,
+			$reg->titulo,
+			$reg->nombre,
+			$reg->app,
+			$reg->apm,
+			$reg->emp_o_ins,
+			$reg->telefono_particular,
+			$reg->comentarios,
+			$reg->email,
+			$reg->clave_asociada,
+			date("Y-m-d H:i:s"),
+			$reg->status,
+			$reg->id_registro
+		);
+
+		if ($this->db->hQuery($qry)) {
+			if ($this->db->hAffectedRows() == 1) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public function asistencia($registerId) {
+		if (!empty($registerId)) {
+			$now = date("Y-m-d H:i:s");
+
+			$qry = 'UPDATE smc_reg_CIPE14 SET 
+				impresion_gafete = 1,
+				fecha_impresion_gafete = "' . $now . '",
+				impresion_total_gafete = impresion_total_gafete + 1
+				WHERE id_registro = ' . $registerId . ' LIMIT 1';
+
+			if ($this->db->hQuery($qry)) {
+				if ($this->db->hAffectedRows() == 1) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+
+	public function validarFolio($folio) {
+		$register = false;
+		if (!empty($folio)) {
+			$qry = "SELECT * 
+				FROM smc_reg_CIPE14 
+				WHERE clave_asociada = " . $folio .  "
+				LIMIT 1";
+
+			if ($this->db->hQuery($qry)) {
+				if ($this->db->hNumRows() == 1) {
+					$register = true;
+				}
+			}
+		}
+		return $register;
+	}
+
+	public function validarFolioId($folio, $id) {
+		$register = false;
+		
+		$qry = "SELECT * 
+			FROM smc_reg_CIPE14 
+			WHERE clave_asociada = " . $folio .  "
+			AND id_registro <> " . $id .  "
+			LIMIT 1";
+
+		if ($this->db->hQuery($qry)) {
+			if ($this->db->hNumRows() == 1) {
+				$register = true;
+			}
+		}
+
+		return $register;
+	}
+
+	public function prueba($post) {
 		$qry = "INSERT INTO prueba SET 
 			reader_name = '%s',
 			mac_address = '%s',
